76ea9cfe6eb2c91eaa2aadb8a3cc00baf6d95fdd
krutideepanpanda <krutideepan123@gmail.com>
2021-10-04 18:58:13 +0530

    Created VOP2_ALU

---
+++ src/VOP2_ALU.v
@@ -0,0 +1,18 @@
+`include "VOP2.sv"
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: IEEE NITK
+// Engineer: Kruti Deepan Panda
+// 
+// Create Date: 04.10.2021 18:48:26
+// Design Name: ALU
+// Module Name: VOP2_ALU
+// Project Name: RDNA2 Compute Unit 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module VOP2_ALU(
+input wire instruction[31:0]
+    );
+    
+endmodule
