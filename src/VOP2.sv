f77ded4ae8b67c5c605e09e98cd5e63ad244f799
krutideepanpanda <krutideepan123@gmail.com>
2021-10-06 09:37:09 +0530

    Added placeholders

--- src/VOP2.sv
+++ src/VOP2.sv
@@ -1,41 +1,45 @@
 `ifndef VOP2
 `define VOP2
 
-`define V_ADD_{F16, F32}	 	64'b0
-`define V_ADD_CO_CI_U32			64'b0
-`define V_ADD_NC_U32			64'b0
-`define V_AND_B32 			64'b0
-`define V_ASHRREV_B32			64'b0
-`define V_CNDMASK_B32 			64'b0
-`define V_CVT_PKRTZ_F16_F32		64'b0
-`define V_FMAAK_F16 			64'b0
-`define V_FMAAK_F32			64'b0
-`define V_FMAC_F16			64'b0
-`define V_FMAC_F32			64'b0
-`define V_FMAMK_F16			64'b0
-`define V_FMAMK_F32 			64'b0
-`define V_LDEXP_F16 			64'b0
-`define V_LSHLREV_B32			64'b0
-`define V_LSHRREV_B32 			64'b0
-`define V_MAC_{ F16,F32} 		64'b0
-`define V_MAX_{ F16, F32,I32,U32} 	64'b0
-`define V_MIN_{ F16, F32,I32,U32} 	64'b0
-`define V_MUL_{F16, F32}		64'b0
-`define V_MUL_HI_I32_I24		64'b0
-`define V_MUL_HI_U32_U24		64'b0
-`define V_MUL_I32_I24 			64'b0
-`define V_MUL_LEGACY_F32		64'b0
-`define V_MUL_U32_U24			64'b0
-`define V_OR_B32			64'b0
-`define V_SUB_{F16, F32} 		64'b0
-`define V_SUB_CO_CI_U32			64'b0
-`define V_SUB_NC_U32			64'b0
-`define V_SUBREV_{F16, F32}		64'b0
-`define V_SUBREV_CO_CI_U32		64'b0
-`define V_SUBREV_NC_U32			64'b0
-`define V_XNOR_B32			64'b0
-`define V_XOR_B32 			64'b0
-`define V_DOT2C_F32_F16 		64'b0
-`define V_DOT4C_I32_I8			64'b0
+`define V_ADD_F16       	 	32'b0
+`define V_ADD_F32       	 	32'b0
+`define V_ADD_CO_CI_U32			32'b0
+`define V_ADD_NC_U32			32'b0
+`define V_AND_B32    			32'b0
+`define V_ASHRREV_B32			32'b0
+`define V_CNDMASK_B32 			32'b0
+`define V_CVT_PKRTZ_F16_F32		32'b0
+`define V_FMAAK_F16 			32'b0
+`define V_FMAAK_F32			    32'b0
+`define V_FMAC_F16			    32'b0
+`define V_FMAC_F32			    32'b0
+`define V_FMAMK_F16			    32'b0
+`define V_FMAMK_F32 			32'b0
+`define V_LDEXP_F16 			32'b0
+`define V_LSHLREV_B32			32'b0
+`define V_LSHRREV_B32 			32'b0
+`define V_MAC_{ F16,F32} 		32'b0
+`define V_MAX_{ F16, F32,I32,U32} 	32'b0
+`define V_MIN_{ F16, F32,I32,U32} 	32'b0
+`define V_MUL_F16       		32'b0
+`define V_MUL_F32       		32'b0
+`define V_MUL_HI_I32_I24		32'b0
+`define V_MUL_HI_U32_U24		32'b0
+`define V_MUL_I32_I24 			32'b0
+`define V_MUL_LEGACY_F32		32'b0
+`define V_MUL_U32_U24			32'b0
+`define V_OR_B32		      	32'b0
+`define V_SUB_F16        		32'b0
+`define V_SUB_F32        		32'b0
+`define V_SUB_CO_CI_U32			32'b0
+`define V_SUB_NC_U32			32'b0
+`define V_SUBREV_F16    		32'b0
+`define V_SUBREV_F32    		32'b0
+`define V_SUBREV_CO_CI_U32		32'b0
+`define V_SUBREV_NC_U32			32'b0
+`define V_XNOR_B32  			32'b0
+`define V_XOR_B32    			32'b0
+`define V_DOT2C_F32_F16 		32'b0
+`define V_DOT4C_I32_I8			32'b0
 
 `endif
--- src/VOP2_ALU.v
+++ src/VOP2_ALU.v
@@ -15,4 +15,52 @@ module VOP2_ALU(
 input wire instruction[31:0]
     );
     
+    wire src0[8:0] = instruction[8:0] ;
+    wire vsrc1[7:0]= instruction[16:9] ;
+    wire vsrc2[7:0]= instruction[24:7] ;
+    wire op[5:0]= instruction[30:25] ;
+    
+    always @(*)
+    begin
+        case(op)
+            V_DOT2C_F32_F16 :begin
+                                //Dot product of packed FP16 values, accumulate with destination.
+                                //D.f32 = S0.f16[0] * S1.f16[0] + S0.f16[1] * S1.f16[1] + D.f32.
+                            end
+            V_ADD_F32       :begin
+                                //Add two single-precision values. 0.5ULP precision, denormals are supported.
+                                //D.f32 = S0.f32 + S1.f32.
+                            end
+            V_SUB_F32       :begin
+                                //Subtract the second single-precision input from the first input.
+                                //D.f32 = S0.f32 - S1.f32.
+                            end
+            V_SUBREV_F32    :begin
+                                //Subtract the first single-precision input from the second input.
+                                //D.f32 = S1.f32 - S0.f32.
+                            end
+            V_MUL_LEGACY_F32:begin
+                                //Multiply two single-precision values. Follows DX9 rules where 0.0 times anything produces 0.0 (this is not IEEE compliant).
+                                //D.f32 = S0.f32 * S1.f32. // DX9 rules, 0.0*x = 0.0
+                            end
+            V_MUL_F32       :begin
+                                //Multiply two single-precision values. 0.5ULP precision, denormals are supported.
+                                //D.f32 = S0.f32 * S1.f32.
+                            end
+            V_MUL_I32_I24   :begin
+                                //Multiply two signed 24-bit integers and store the result as a
+                                //signed 32-bit integer. This opcode is as efficient as basic
+                                //single-precision opcodes since it utilizes the single-precision
+                                //floating point multiplier. See also V_MUL_HI_I32_I24.
+                                //D.i32 = S0.i24 * S1.i24.
+                            end
+            V_MUL_HI_I32_I24:begin
+                                //Multiply two signed 24-bit integers and store the high 32 bits
+                                //of the result as a signed 32-bit integer. See also
+                                //V_MUL_I32_I24.
+                                //D.i32 = (S0.i24 * S1.i24)>>32;
+                            end
+        endcase
+    end
+    
 endmodule
