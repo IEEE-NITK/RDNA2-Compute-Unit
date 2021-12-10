5db0bbc1474fa7d510772066d06fbdb8de09d0e5
krutideepanpanda <krutideepan123@gmail.com>
2021-12-10 17:00:41 +0530

    Made a 32bit CSA

--- src/CSA.v
+++ src/CSA.v
@@ -3,26 +3,32 @@
 `timescale 1ns / 1ps
 
 module CSA(
-    input wire[1:0] A,
-    input wire[1:0] B,
-    output wire[1:0] Sum,
+    input wire[31:0] A,
+    input wire[31:0] B,
+    output wire[31:0] Sum,
     output wire Cout
     );
 
-wire[2:0] temp_c2, temp_sum1, temp_c1;
+wire[32:0] temp_c2, temp_sum1, temp_c1;
 
-assign temp_sum1[2] = 1'b0;
+assign temp_sum1[32] = 1'b0;
 assign temp_c1[0] = 1'b0;
 
-fulladder temp_bit0(.A(A[0]), .B(B[0]), .Cin(1'b0), .Sum(temp_sum1[0]), .Cout(temp_c1[1]));
-fulladder temp_bit1(.A(A[1]), .B(B[1]), .Cin(1'b0), .Sum(temp_sum1[1]), .Cout(temp_c1[2]));
-
-//fulladder bit0(.A(temp_sum1[0]), .B(1'b0), .Cin(1'b0), .Sum(temp_sum2[0]), .Cout(temp_c2[0]));
-//fulladder bit1(.A(temp_sum1[1]), .B(temp_c1[0]), .Cin(temp_c2[0]), .Sum(temp_sum2[1]), .Cout(temp_c2[1]));
-//fulladder bit2(.A(1'b0), .B(temp_c1[1]), .Cin(temp_c2[1]), .Sum(temp_sum2[2]), .Cout(temp_c2[2]));
-
-fulladder bit0(.A(temp_sum1[0]), .B(1'b0), .Cin(1'b0), .Sum(Sum[0]), .Cout(temp_c2[0]));
-fulladder bit1(.A(temp_sum1[1]), .B(temp_c1[1]), .Cin(temp_c2[0]), .Sum(Sum[1]), .Cout(temp_c2[1]));
-fulladder bit2(.A(1'b0), .B(temp_c1[2]), .Cin(temp_c2[1]), .Sum(Cout), .Cout(temp_c2[2]));
+genvar i;
+generate
+    for (i=0; i<=31; i=i+1)
+    begin
+        fulladder temp_bit(.A(A[i]), .B(B[i]), .Cin(1'b0), .Sum(temp_sum1[i]), .Cout(temp_c1[(i+1)]));
+    end
+    
+    fulladder bit0(.A(temp_sum1[0]), .B(1'b0), .Cin(1'b0), .Sum(Sum[0]), .Cout(temp_c2[0]));
+    
+    for (i=1;i<=31;i = i+1)
+    begin
+        fulladder bit1(.A(temp_sum1[i]), .B(temp_c1[i]), .Cin(temp_c2[(i-1)]), .Sum(Sum[i]), .Cout(temp_c2[i]));
+    end
+    
+    fulladder bit32(.A(1'b0), .B(temp_c1[32]), .Cin(temp_c2[31]), .Sum(Cout), .Cout(temp_c2[32]));
+endgenerate
    
 endmodule
--- tb/CSA_test.v
+++ tb/CSA_test.v
@@ -2,9 +2,9 @@
 
 module CSA_test;
 
-reg[1:0] A;
-reg[1:0] B;
-wire[1:0] Sum;
+reg[31:0] A;
+reg[31:0] B;
+wire[31:0] Sum;
 wire Cout;
 
 CSA temp(.A(A), .B(B), .Sum(Sum), .Cout(Cout));
@@ -12,32 +12,23 @@ CSA temp(.A(A), .B(B), .Sum(Sum), .Cout(Cout));
 initial
 begin
     #(10);
-    A = 2'b10;
-    B = 2'b01;
+    A = 32'd10;
+    B = 32'd01;
+    #(10);
+    $display("A = %d, B = %d, Sum = %d, Cout = %d", A, B, Sum, Cout);
     
     #(10);
-    A = 2'b11;
-    B = 2'b01;
+    A = 32'd200;
+    B = 32'd6969;
+    #(10);
+    $display("A = %d, B = %d, Sum = %d, Cout = %d", A, B, Sum, Cout);
     
     #(10);
-    A = 2'b01;
-    B = 2'b10;
-    
+    A = 32'hffffffff;
+    B = 32'd01;
     #(10);
-    A = 2'b00;
-    B = 2'b01;
+    $display("A = %d, B = %d, Sum = %d, Cout = %d", A, B, Sum, Cout);
     
-    #(10);
-    A = 2'b01;
-    B = 2'b01;    
-    
-    #(10);
-    A = 2'b01;
-    B = 2'b11;
-    
-    #(10);
-    A = 2'b00;
-    B = 2'b00;
 end
 
 endmodule
