04cb65c6f5970407ced4378a23fe0485784561d6
krutideepanpanda <krutideepan123@gmail.com>
2022-01-14 17:38:37 +0530

    Floating point adder finished

--- src/Vector ALU/float_adder_32.v
+++ src/Vector ALU/float_adder_32.v
@@ -5,7 +5,8 @@ module float_adder_32(
     input wire[31:0] B,
     output reg[31:0] out,
     output reg NaN_flag,
-    output reg overflow_flag
+    output reg overflow_flag,
+    output reg neg_flag
     );
     
 function [23:0] complement2 (   input hidden_bit,
@@ -21,12 +22,14 @@ endfunction
     wire[7:0] exp_A;
     wire[22:0] fraction_A;
     reg hidden_bit_A;
+    reg[23:0] significand_A;
     
     // For B --------------------------------------------------------------------------------------------------------------------------------------
     wire sign_B;
     wire[7:0] exp_B;
     wire[22:0] fraction_B;
     reg hidden_bit_B;
+    reg[23:0] significand_B;
     
     // For output --------------------------------------------------------------------------------------------------------------------------------------
     reg sign_out;
@@ -66,7 +69,7 @@ endfunction
     subtractor_8b sub(.A(exp_A), .B(exp_B), .diff(diff_exp), .Borrow(Borrow));
     RightShift shift(.A(significand_shift), .amt(diff_amt), .out(significand_shifted));
     CSA_24b adder(.A(significand_non_shift), .B(significand_shifted), .Sum(significand_out), .Cout(Cout));
-    normalizer norm(.significand(significand_out), .exp(exp_out), .cout(Cout), .norm_sig(norm_significand), .norm_exp(norm_exp));
+    normalizer norm(.significand(significand_out), .exp(exp_out), .cout(Cout), .neg_flag(neg_flag), .norm_sig(norm_significand), .norm_exp(norm_exp));
     
     reg [23:0] temp_comp ;
     
@@ -164,6 +167,30 @@ always @(*)
         else
         begin
         $display("in normal");
+        
+            if (sign_A == sign_B)
+            begin
+            $display("same sign");
+                neg_flag <= 1'b0;
+                sign_out <= sign_A;
+                
+                significand_A = { hidden_bit_A, fraction_A};
+                significand_B = { hidden_bit_B, fraction_B};
+            end
+            // diff sign            
+            else if (sign_A != sign_B)
+            begin
+            $display("diff sign");
+                neg_flag <= 1'b1;
+                if (sign_A == 1'b1)
+                begin
+                    significand_A = complement2(hidden_bit_A, fraction_A);
+                end
+                else if(sign_B == 1'b1)
+                begin
+                    significand_B = complement2(hidden_bit_B, fraction_B);
+                end
+            end
 
             //determinning output exponent and calculatiing exponent difference
             if (Borrow == 1'b1)
@@ -171,55 +198,45 @@ always @(*)
                 
                 diff_amt <= 9'd256 - diff_exp;
                 
+                sign_out = sign_B;
+                
                 $display("A shifted, diff_exp = %d, diff_amt = %d", diff_exp, diff_amt);
                 $display("exp_A = %d, exp_B = %d", exp_A, exp_B);
                 //exp_B is bigger
-                frac_shift <= fraction_A;
-                significand_shift <= {hidden_bit_A, frac_shift};
+                //frac_shift <= fraction_A;
+                //significand_shift <= {hidden_bit_A, frac_shift};
+                significand_shift = significand_A;
                 
                 //frac_non_shift = fraction_B;
-                significand_non_shift <= { hidden_bit_B, significand_non_shift};
+                //significand_non_shift <= { hidden_bit_B, significand_non_shift};
+                significand_non_shift = significand_B;
                 
                 //exponent and sign
                 exp_out <= exp_B;
             end
             else if (Borrow == 1'b0)
             begin
+                
+                sign_out = sign_A;
+                
                 //exp_A is bigger
                 diff_amt <= diff_exp;
                 $display("B shifted, diff_exp = %d, diff_amt = %d", diff_exp, diff_amt);
                 $display("exp_A = %d, exp_B = %d", exp_A, exp_B);
-                frac_shift <= fraction_B;
-                significand_shift <= { hidden_bit_B, frac_shift};
                 
-                frac_non_shift <= fraction_A;
-                significand_non_shift <= { hidden_bit_A, frac_non_shift};
+                //frac_shift <= fraction_B;
+                //significand_shift <= { hidden_bit_B, frac_shift};
+                significand_shift = significand_B;
                 
-                exp_out <= exp_A;
-            end
+                //frac_non_shift <= fraction_A;
+                //significand_non_shift <= { hidden_bit_A, frac_non_shift};
+                significand_non_shift = significand_A;
+                
+                exp_out = exp_A;
+            end        
             
-            if (sign_A == sign_B)
-            begin
-            $display("same sign");
-                sign_out <= sign_A;
-            end
-            // diff sign            
-            else if (sign_A != sign_B)
-            begin
-            $display("diff sign");
-                if (sign_A == 1'b1)
-                begin
-                    significand_shift <= complement2(hidden_bit_A, fraction_A);
-                end
-                else if(sign_B == 1'b1)
-                begin
-                    significand_shift <= complement2(hidden_bit_B, fraction_B);
-                end
-            end
-        
-            
-            fraction_out <= norm_significand[22:0];
-            out <= {sign_out, norm_exp, fraction_out};
+            fraction_out = norm_significand[22:0];
+            out = {sign_out, norm_exp, fraction_out};
         end
     end
     
--- src/Vector ALU/normalizer.v
+++ src/Vector ALU/normalizer.v
@@ -4,6 +4,7 @@ module normalizer(
     input [23:0] significand,
     input [7:0] exp,
     input cout,
+    input neg_flag,
     output reg [23:0] norm_sig,
     output reg [7:0] norm_exp
     );
@@ -11,20 +12,45 @@ module normalizer(
     wire[24:0] temp;
     wire[24:0] temp_sig;
     
+    reg[7:0] temp_z;
+    
     assign temp = {cout, significand};
     assign temp_sig = temp >> 1;    
     
     always @(*)
     begin
-        if (cout == 1'b1)
+        if (neg_flag == 1'b0)
         begin
-            norm_sig <= temp[23:0];
-            norm_exp <= exp + 1;
-        end    
-        else
+            if (cout == 1'b1)
+            begin
+                norm_sig <= temp[23:0];
+                norm_exp <= exp + 1;
+            end    
+            else
+            begin
+                norm_sig <= significand;
+                norm_exp <= exp;
+            end
+        end
+        else if (neg_flag == 1'b1)
         begin
-            norm_sig <= significand;
-            norm_exp <= exp;
+            if (cout == 1'b1)
+            begin
+                norm_sig = significand;
+                temp_z = 8'b0;
+                while (norm_sig[23] != 1'b1)
+                begin
+                    norm_sig = norm_sig << 1;
+                    temp_z = temp_z + 1'b1;
+                end
+                
+                norm_exp = exp - temp_z;
+            end    
+            else
+            begin
+                norm_sig <= ~(significand) + 1;
+                norm_exp <= exp;
+            end
         end
     end
     
